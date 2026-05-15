444856d854e7df5c27cfd8325ffab7e7d9f81b1c
yuyu <sishichulu@gmail.com>
2026-05-15 22:54:40 +0300

    deploy: remove excess import

--- agtech-app/src/App.tsx
+++ agtech-app/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { PredictionForm } from './components/PredictionForm';
 import { ResultView } from './components/ResultView';
